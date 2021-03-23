1a8b7acf9857e0f53658c539025ec802beac5918
Suyash L <suyashblal@gmail.com>
2021-03-23 20:47:43 +0000

    added calc

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ a = 15
 b = 16 
 c = 17
 
-print(a*c)
-print(16^17)
-print(15 / 17)
+
+print('Addition of a+b=' + str(a+b))
+print('B to the power of C'+ str(b**c))
+print('A divided by C a/c' + str(a/c))
