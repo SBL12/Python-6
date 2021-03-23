b7223d97b49a20abc829dc29fea79c1140e8a097
Suyash L <suyashblal@gmail.com>
2021-03-23 20:57:15 +0000

    This is the little rectangle game.

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ c = 17
 print('Addition of a+b=' + str(a+b))
 print('B to the power of C'+ str(b**c))
 print('A divided by C a/c' + str(a/c))
+
+L = 50
+W = int(input("Enter the width of the rectangle"))
+A = L*W
+P = L*2+W*2
+print("The area is" + str(A))
+print("The perimeter is" + str(P))
